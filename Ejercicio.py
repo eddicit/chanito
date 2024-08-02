8b28599836068bd2e91471b637874971a1c0c4fc
eddicit <eddymar238@gmail.com>
2024-08-02 16:55:07 -0600

    Primera actualizacion

---
+++ Ejercicio.py
@@ -0,0 +1,11 @@
+"""#pide lemguaje 
+language = input('¿Cual es tu lenguaje favorito?')
+
+print("tu lenguaje favorito rd, ", language)"""
+
+num1 = float(input("Ingresee el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+resultado = num1 + num2
+
+print(resultado)
